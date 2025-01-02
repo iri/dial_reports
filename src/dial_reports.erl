ea26b5933014c4efcc1305e6df254a41f27305d1
iri <ram3a12@gmail.com>
2025-01-02 23:37:24 +0300

    File reading added

--- src/dial_reports.erl
+++ src/dial_reports.erl
@@ -8,25 +8,34 @@
 %%====================================================================
 
 %% escript Entry point
-main(Args) ->
-    Filename = lists:nth(1, Args),
-    {ok, File} = file:open(Filename, [read]),
-    read_lines(File),
-    file:close(File),
+main(_Args) ->
+    process_file("priv/list_erl.txt"),
+    process_file("priv/list_hrl.txt"),
+    process_file("priv/list_c.txt"),
+    process_file("priv/list_h.txt"),
     erlang:halt(0).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
-read_lines(File) ->
+process_file(Filename) ->
+    Proc = fun(Tokens) -> 
+                io:format("~p~n",[Tokens])
+           end,
+    {ok, File} = file:open(Filename, [read]),
+    read_lines(File, Proc),
+    file:close(File).
+
+read_lines(File, Proc) ->
     case io:get_line(File, "") of
         eof ->
             ok;
         {error, Reason} ->
             throw({error, Reason});
         Line ->
-            io:format("~p~n",[Line]),
-            read_lines(File)
+            [_|Tokens] = string:split(Line, "/", all),
+            Proc(Tokens),
+            read_lines(File, Proc)
     end.
 
