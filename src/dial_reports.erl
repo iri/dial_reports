73376bdf16afb07df57d4a78208dfc6c890b09d1
Ingars Ribners <ram3a12@gmail.com>
2025-01-02 23:10:00 +0300

    File load function started

--- src/dial_reports.erl
+++ src/dial_reports.erl
@@ -9,9 +9,24 @@
 
 %% escript Entry point
 main(Args) ->
-    io:format("Args: ~p~n", [Args]),
+    Filename = lists:nth(1, Args),
+    {ok, File} = file:open(Filename, [read]),
+    read_lines(File),
+    file:close(File),
     erlang:halt(0).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+read_lines(File) ->
+    case io:get_line(File, "") of
+        eof ->
+            ok;
+        {error, Reason} ->
+            throw({error, Reason});
+        Line ->
+            io:format("~p~n",[Line]),
+            read_lines(File)
+    end.
+
